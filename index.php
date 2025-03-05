956b3e0c31a0883559685a1c3c017de337d5a135
Justin Soulard <e6239575@cmaisonneuve.qc.ca>
2025-03-05 17:48:50 -0500

    Soustrait du fichier de wordpress dans mon depot github

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+    <?php get_header(); ?>
+    <h1>-------- INDEX.PHP ----------</h1>
+    <section class="populaire">
+        <div class="global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <div><?php the_content() ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+   
+</body>
+</html>
