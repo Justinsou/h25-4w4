79fc147d4987948ddaa675b0fc4c8df66d495070
Justin Soulard <e6239575@cmaisonneuve.qc.ca>
2025-03-31 14:08:14 -0400

    Entete adapte pour mobile, tablette et ordinateur

--- 404.php
+++ 404.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 <?php 
     $erreur_404_background = get_theme_mod('erreur_404_background', '');
+    $Texte_404 = get_theme_mod('Texte_404', '');
+    $Bouton_404 = get_theme_mod('Bouton_404', '');
  ?>
 
 <main >
   <p class="erreur_barre erreur_barre_haut ">//////////////////////////////////////////////////////////////////////////////////////////</p>
   <section class="erreur_404"  style="background-image: url('<?= $erreur_404_background ?>')">
     <h1>Erreur 404</h1>
-    <h3>La page que vous cherchez n'existe pas.</h3>
-    <p class="bouton_acceuil">
-      <a href="<?php echo home_url(); ?>">Revenir à l'accueil</a> <!-- Bouton avec lien vers l'accueil -->
+    <h3><?= $Texte_404 ?></h3>
+    <p class="bouton_acceuil"> 
+      <a href="<?php echo home_url(); ?>"><?= $Bouton_404 ?></a> <!-- Bouton avec lien vers l'accueil -->
     </p>
   </section>
   <p class="erreur_barre erreur_barre_bas">//////////////////////////////////////////////////////////////////////////////////////////</p>
--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Club de voyage</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=menu" />
     <!-- link rel="stylesheet" href="normalize.css" -->
     <!-- link rel="stylesheet" href="style.css" -->
     <?php wp_head() ?>
@@ -12,6 +13,7 @@
     <header>
         <div class="entete">
             <figure class="entete__logo">
+            
             <?php
             if (function_exists('the_custom_logo')) {
                 the_custom_logo();
@@ -25,7 +27,11 @@
                     'container_class' => 'entete__menu'
                 )); ?>
                 <?php get_search_form() ?>
-
+              
             </div> <!-- fin entete__navigation  -->
+            
+            <span class="material-symbols-outlined">
+                menu
+                </span>
         </div>
     </header>
