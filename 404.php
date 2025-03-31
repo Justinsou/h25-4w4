361c8fb051ceec55a45f0f5e9bb5220d8eefe5ce
Justin Soulard <e6239575@cmaisonneuve.qc.ca>
2025-03-31 14:35:40 -0400

    Ajout du titre et du texte de erreur 404

--- 404.php
+++ 404.php
@@ -1,20 +1,31 @@
 <?php get_header(); ?>
 <?php 
     $erreur_404_background = get_theme_mod('erreur_404_background', '');
+
+    $Titre_404 = get_theme_mod('Titre_404', '');
     $Texte_404 = get_theme_mod('Texte_404', '');
     $Bouton_404 = get_theme_mod('Bouton_404', '');
  ?>
 
 <main >
-  <p class="erreur_barre erreur_barre_haut ">//////////////////////////////////////////////////////////////////////////////////////////</p>
   <section class="erreur_404"  style="background-image: url('<?= $erreur_404_background ?>')">
-    <h1>Erreur 404</h1>
-    <h3><?= $Texte_404 ?></h3>
-    <p class="bouton_acceuil"> 
-      <a href="<?php echo home_url(); ?>"><?= $Bouton_404 ?></a> <!-- Bouton avec lien vers l'accueil -->
-    </p>
+    <div class="encadrer">
+      <div class="titre" ><h1><?= $Titre_404 ?></h1></div>
+      <div class="texte" ><p><?= $Texte_404 ?></p></div>
+      <p class="bouton_acceuil"> 
+        <a href="<?php echo home_url(); ?>"><?= $Bouton_404 ?></a> <!-- Bouton avec lien vers l'accueil -->
+      </p>
+
+      <div class="erreur_404__navigation">
+                <?php wp_nav_menu(array(
+                    'menu' => 'erreur_404',
+                    'container' => 'nav',
+                    'container_class' => 'entete__menu'
+                )); ?>
+            </div> 
+    </div>
+   
   </section>
-  <p class="erreur_barre erreur_barre_bas">//////////////////////////////////////////////////////////////////////////////////////////</p>
 </main>
 
 <?php get_footer(); ?>
